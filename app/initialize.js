bcc31ac0a865c5540cabe134082d3a74d3230b6d
Kazzkiq <claudiohw@gmail.com>
2018-01-11 00:35:50 -0200

    Adding dependencies for PPC address generation

--- app/initialize.js
+++ app/initialize.js
@@ -3,6 +3,12 @@
  */
 
 import Routes from './routes';
+import buffer from 'buffer';
+
+// Mimic Node.js Buffer from Browser's Uint8Array
+// This is needed for easier Peercoin address generation
+window.Buffer = buffer.Buffer;
+Error.captureStackTrace = err => console.log;
 
 document.addEventListener('DOMContentLoaded', main);
 

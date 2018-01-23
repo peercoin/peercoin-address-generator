e0ee37f921084f6252d6a18daa03445fc48ee137
Kazzkiq <claudiohw@gmail.com>
2018-01-22 23:07:28 -0200

    Adding "share" functionalities

--- app/pages/wallet/wallet.handler.js
+++ app/pages/wallet/wallet.handler.js
@@ -1,16 +1,34 @@
+import roadtrip from 'roadtrip';
 import Wallet from './wallet.page.svelte';
 
 export default class WalletHandler {
   get route() {
     return {
       enter(current, previous) {
-        this.component = new Wallet({
-          target: document.getElementById('app')
-        });
-        document.body.style.overflow = 'hidden';
+        const pubAddr = window.store.get('publicAddress');
+        const privKey = window.store.get('privateKey');
+
+        // If no address generated, redirect to home
+        if (!pubAddr || !privKey) {
+          roadtrip.goto('/');
+        } else {
+          // Else, load view
+          this.component = new Wallet({
+            target: document.getElementById('app')
+          });
+
+          // Disable overflow to prevent scrolling
+          // while giving seed by touch
+          document.body.style.overflow = 'hidden';
+        }
       },
       leave(current, previous) {
-        this.component.destroy();
+        if(this.component) {
+          this.component.destroy();
+        }
+        
+        // Enable overflow for other views
+        document.body.style.overflow = '';
       }
     }
   }
